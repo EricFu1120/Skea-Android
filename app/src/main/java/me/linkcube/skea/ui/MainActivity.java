e89749352077882e16799abde925b0b50495158d
Ervin Wang <orange.work@qq.com>
2014-10-25 22:52:37 +0800

    修正“测试结果页面”

--- app/src/main/java/me/linkcube/skea/ui/MainActivity.java
+++ app/src/main/java/me/linkcube/skea/ui/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
 import me.linkcube.skea.R;
 
 public class MainActivity extends Activity {
