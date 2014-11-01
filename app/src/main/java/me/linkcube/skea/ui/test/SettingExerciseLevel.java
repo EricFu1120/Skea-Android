5221f07f9f12c8295a63911923d30128926bc4b3
cxcbupt <1198066235@qq.com>
2014-11-01 23:38:08 +0800

    增加启动设置训练强度入口并接收用户设置的训练强度

--- app/src/main/java/me/linkcube/skea/ui/test/SettingExerciseLevel.java
+++ app/src/main/java/me/linkcube/skea/ui/test/SettingExerciseLevel.java
@@ -1,12 +1,23 @@
 package me.linkcube.skea.ui.test;
 
+
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioGroup;
+
 import me.linkcube.skea.R;
+import me.linkcube.skea.ui.user.TestPelvicMuscleResultActivity;
 
 public class SettingExerciseLevel extends ActionBarActivity {
+    //训练强度
+
+    private String exerciseLevelString = "Level 4";
+    //控件声明
+    private RadioGroup exerciseLevel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +26,53 @@ public class SettingExerciseLevel extends ActionBarActivity {
         //ActionBar实现后退导航
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //返回用户设置的训练强度
+        Intent resultIntent=new Intent();
+        resultIntent.putExtra(TestPelvicMuscleResultActivity.EXERCISE_LEVEL,exerciseLevelString);
+        setResult(RESULT_OK,resultIntent);
+    }
+
+    public void init() {
+        //得到控件
+        exerciseLevel = (RadioGroup) findViewById(R.id.exercise_level);
+        //注册事件
+        exerciseLevel.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.level1_rb:
+                        exerciseLevelString = "Level 1";
+
+
+                        break;
+                    case R.id.level2_rb:
+                        exerciseLevelString = "Level 2";
+
+                        break;
+                    case R.id.level3_rb:
+
+                        exerciseLevelString = "Level 3";
+                        break;
+                    case R.id.level4_rb:
+
+                        exerciseLevelString = "Level 4";
+                        break;
+                    case R.id.level5_rb:
+
+                        exerciseLevelString = "Level 5";
+                        break;
+                    default:
+
+
+                }
+            }
+        });
     }
 
 
@@ -35,5 +93,8 @@ public class SettingExerciseLevel extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+
     }
+
+
 }
--- app/src/main/java/me/linkcube/skea/ui/user/TestPelvicMuscleResultActivity.java
+++ app/src/main/java/me/linkcube/skea/ui/user/TestPelvicMuscleResultActivity.java
@@ -1,17 +1,40 @@
 package me.linkcube.skea.ui.user;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
 import me.linkcube.skea.R;
+import me.linkcube.skea.ui.test.SettingExerciseLevel;
 
 public class TestPelvicMuscleResultActivity extends Activity {
+    public  static final String EXERCISE_LEVEL = "me.linkcube.skea.ui.user.TestPelvicMuscleResultActivity.Exercise_level";
+    private static final int SETTING_LEVEL_REQUEST_CODE = 1;
+
+
+    private TextView exerciseLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_pelvic_muscle_result);
+    }
+
+    private void init() {
+        exerciseLevel = (TextView) findViewById(R.id.level);
+    }
+
+    /**
+     * 启动设置训练强度
+     */
+    private void startSettingExerciseLevel() {
+        Intent intent = new Intent();
+        intent.setClass(getApplicationContext(), SettingExerciseLevel.class);
+        startActivityForResult(intent, SETTING_LEVEL_REQUEST_CODE);
+
     }
 
 
@@ -32,5 +55,25 @@ public class TestPelvicMuscleResultActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                //得到训练强度值
+                if (resultCode == RESULT_OK) {
+
+                    exerciseLevel.setText(data.getStringExtra(EXERCISE_LEVEL));
+
+
+                }
+
+
+                break;
+            default:
+
+        }
     }
 }
