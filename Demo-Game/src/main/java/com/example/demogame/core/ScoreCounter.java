1d7386cf02469e444aaed315b32959b8ba3d9ee9
Ervin Wang <orange.work@qq.com>
2014-11-01 09:15:09 +0800

    增加了动画工厂类库，并添加了一些资源文件

---
+++ Demo-Game/src/main/java/com/example/demogame/core/ScoreCounter.java
@@ -0,0 +1,8 @@
+package com.example.demogame.core;
+
+/**
+ * Created by Ervin on 14/11/1.
+ */
+public class ScoreCounter {
+
+}
---
+++ Demo-Game/src/main/java/com/example/demogame/view/BarViewAnimator.java
@@ -0,0 +1,14 @@
+package com.example.demogame.view;
+
+import android.view.View;
+
+/**
+ * Created by Ervin on 14/10/31.
+ */
+public class BarViewAnimator extends BaseViewAnimator {
+
+    @Override
+    protected void prepare(View target) {
+
+    }
+}
