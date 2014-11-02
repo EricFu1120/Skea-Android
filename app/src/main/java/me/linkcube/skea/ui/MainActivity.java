6ff4a0470cb29b109319cb85ba04f0845baa5e5b
cxcbupt <1198066235@qq.com>
2014-11-02 23:59:59 +0800

    修改MainActivity及其布局，增加“Info”,“Records”以及"Me"的启动方法

--- app/src/main/java/me/linkcube/skea/ui/MainActivity.java
+++ app/src/main/java/me/linkcube/skea/ui/MainActivity.java
@@ -1,20 +1,84 @@
 package me.linkcube.skea.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import me.linkcube.skea.R;
+import me.linkcube.skea.ui.info.InformationActivity;
+import me.linkcube.skea.ui.record.RecordActivity;
+import me.linkcube.skea.ui.user.UserInfoActivity;
 
 public class MainActivity extends Activity {
+
+
+    //声明控件
+    /**
+     * Info
+     */
+    private TextView infoTextView;
+    /**
+     * Records
+     */
+    private TextView recordsTextView;
+    /**
+     * Me
+     */
+    private TextView meTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+
     }
+
+    /**
+     * 初始化控件，注册相应事件
+     */
+    private void init() {
+        infoTextView = (TextView) findViewById(R.id.infoTextView);
+        recordsTextView = (TextView) findViewById(R.id.recordsTextView);
+        meTextView = (TextView) findViewById(R.id.meTextView);
+
+        infoTextView.setOnClickListener(mainTextViewOnClickListener);
+        recordsTextView.setOnClickListener(mainTextViewOnClickListener);
+        meTextView.setOnClickListener(mainTextViewOnClickListener);
+
+
+    }
+
+    View.OnClickListener mainTextViewOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.infoTextView:
+                    startActivity(new Intent().setClass(getApplicationContext(), InformationActivity.class));
+
+                    break;
+
+                case R.id.recordsTextView:
+                    startActivity(new Intent().setClass(getApplicationContext(), RecordActivity.class));
+                    break;
+                case R.id.meTextView:
+                    startActivity(new Intent().setClass(getApplicationContext(), UserInfoActivity.class));
+                    break;
+
+                default:
+
+
+            }
+
+        }
+    };
 
 
     @Override
