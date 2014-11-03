4aad2ae9a3969e267d1a5d168bd1cf520fdf11fb
cxcbupt <1198066235@qq.com>
2014-11-04 00:48:34 +0800

    增加Skea Setting ,Puchase Product和Software Setting 程序入口

--- app/src/main/java/me/linkcube/skea/ui/user/UserInfoActivity.java
+++ app/src/main/java/me/linkcube/skea/ui/user/UserInfoActivity.java
@@ -1,19 +1,82 @@
 package me.linkcube.skea.ui.user;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.net.Uri;
+
+import org.w3c.dom.Text;
+
 import me.linkcube.skea.R;
+import me.linkcube.skea.ui.setting.SettingActivity;
+import me.linkcube.skea.ui.setting.SkeaConfigActivity;
 
 public class UserInfoActivity extends ActionBarActivity {
+    private TextView test_pelvic_muscle;
+    private TextView skea_settings;
+    private TextView purchase_product;
+    private TextView software_settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        init();
+
     }
 
+    /**初始化，并注册相应事件*/
+    private void init(){
+        //得到控件
+        test_pelvic_muscle=(TextView) findViewById(R.id.test_pelvic_muscle);
+        skea_settings=(TextView) findViewById(R.id.skea_settings);
+
+        purchase_product=(TextView) findViewById(R.id.purchase_product);
+
+        software_settings=(TextView) findViewById(R.id.software_settings);
+
+
+
+
+        //注册事件
+        test_pelvic_muscle.setOnClickListener(onUserInfoTextViewClickListener);
+        skea_settings.setOnClickListener(onUserInfoTextViewClickListener);
+
+        purchase_product.setOnClickListener(onUserInfoTextViewClickListener);
+
+        software_settings.setOnClickListener(onUserInfoTextViewClickListener);
+
+
+
+    }
+
+    View.OnClickListener onUserInfoTextViewClickListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.test_pelvic_muscle:
+
+                    break;
+                case R.id.skea_settings:
+                    startActivity(new Intent().setClass(getApplicationContext(), SkeaConfigActivity.class));
+                    break;
+                case R.id.purchase_product:
+                    Uri uri=Uri.parse("http://www.linkcube.me");//网址前没有加http
+                    Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                    startActivity(intent);
+                    break;
+                case R.id.software_settings:
+                    startActivity(new Intent().setClass(getApplicationContext(), SettingActivity.class));
+                    break;
+                default:
+
+            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
