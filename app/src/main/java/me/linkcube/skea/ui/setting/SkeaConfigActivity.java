32fd99c05d9f7a2b07aeba7d372682a7323d542b
cxcbupt <1198066235@qq.com>
2014-11-11 13:08:33 +0800

    Skea Settings 中增加Pressure Sensitivity 和 Feedback Strength数据的本地持久化

--- app/src/main/java/me/linkcube/skea/ui/setting/SkeaConfigActivity.java
+++ app/src/main/java/me/linkcube/skea/ui/setting/SkeaConfigActivity.java
@@ -1,6 +1,7 @@
 package me.linkcube.skea.ui.setting;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -11,6 +12,16 @@ import android.widget.SeekBar;
 import me.linkcube.skea.R;
 
 public class SkeaConfigActivity extends ActionBarActivity {
+    /**Skea Config 本地持久化文件名*/
+    private static final String  SKEA_CONFIG_XML_FILE="Skea_Config_XML_File";
+    /**Feedback Sensitivity Key*/
+    private static final String  SKEA_CONFIG_FEEDBACK_KEY="Feedback_Strength";
+    /**Pressure Sensitivity key*/
+    private static final String  SKEA_CONFIG_PRESSURE_KEY="Pressure_Sensitivity";
+
+    private SharedPreferences mSharedPreferences=null;
+    private SharedPreferences.Editor mEditor=null;
+
     //声明控件
     private SeekBar feedback_sensitive_sb;
     private SeekBar press_sensitive_sb;
@@ -23,16 +34,26 @@ public class SkeaConfigActivity extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         init();
     }
-    /**初始化相关控件，并注册事件*/
+    /**初始化，并注册事件*/
     private void init(){
+
+
+
         //得到相关控件
         feedback_sensitive_sb=(SeekBar) findViewById(R.id.feedback_sensitive_sb);
         press_sensitive_sb=(SeekBar) findViewById(R.id.press_sensitive_sb);
+        //得到之前存储值，并设置SeekBar
+        mSharedPreferences=getSharedPreferences(SKEA_CONFIG_XML_FILE,Activity.MODE_PRIVATE);
+        if(mSharedPreferences!=null){//不是第一次运行
+
+            feedback_sensitive_sb.setProgress(mSharedPreferences.getInt(SKEA_CONFIG_FEEDBACK_KEY,50));
+            press_sensitive_sb.setProgress(mSharedPreferences.getInt(SKEA_CONFIG_PRESSURE_KEY,50));
+
+        }
 
         //注册事件
         feedback_sensitive_sb.setOnSeekBarChangeListener(onSkeaConfigSeekBarChangeListener);
         press_sensitive_sb.setOnSeekBarChangeListener(onSkeaConfigSeekBarChangeListener);
-
     }
 
     SeekBar.OnSeekBarChangeListener onSkeaConfigSeekBarChangeListener=new SeekBar.OnSeekBarChangeListener() {
@@ -48,16 +69,26 @@ public class SkeaConfigActivity extends ActionBarActivity {
 
         @Override
         public void onStopTrackingTouch(SeekBar seekBar) {
+
+            mSharedPreferences=getSharedPreferences(SKEA_CONFIG_XML_FILE,Activity.MODE_PRIVATE);
+            mEditor=mSharedPreferences.edit();
+
             switch (seekBar.getId()){
                 case R.id.feedback_sensitive_sb:
                     //Feedback Strength
                     //todo  ----XXXX----更改设备参数，保存设备参数
                     Log.i("CXC","---feedback_sensitive_sb:"+seekBar.getProgress());
 
+                    mEditor.putInt(SKEA_CONFIG_FEEDBACK_KEY,seekBar.getProgress());
+                    mEditor.commit();
+
                     break;
                 case R.id.press_sensitive_sb:
                     //Pressure Sensitivity
                     //todo  ----XXXX----更改设备参数，保存设备参数
+
+                    mEditor.putInt(SKEA_CONFIG_PRESSURE_KEY,seekBar.getProgress());
+                    mEditor.commit();
 
                     Log.i("CXC","---press_sensitive_sb:"+seekBar.getProgress());
                     break;
