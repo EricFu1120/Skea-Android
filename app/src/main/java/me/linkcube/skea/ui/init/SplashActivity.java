66a6a8d97853194e62abad0adf32178aa8314810
Ervin Wang <orange.work@qq.com>
2014-11-12 15:13:17 +0800

    增加了SplashActivity

--- app/src/main/java/me/linkcube/skea/ui/init/SplashActivity.java
+++ app/src/main/java/me/linkcube/skea/ui/init/SplashActivity.java
@@ -1,36 +1,28 @@
 package me.linkcube.skea.ui.init;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import me.linkcube.skea.R;
 
-public class SplashActivity extends ActionBarActivity {
+import me.linkcube.skea.R;
+import me.linkcube.skea.ui.MainActivity;
+
+public class SplashActivity extends Activity implements Runnable {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.splash, menu);
-        return true;
+        new Thread(this).start();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    public void run() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        return super.onOptionsItemSelected(item);
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
