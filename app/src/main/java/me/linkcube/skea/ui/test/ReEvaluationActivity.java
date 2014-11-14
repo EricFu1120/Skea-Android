de1d19a97dbab863b0cfb8bbe34d66162a26436f
cxcbupt <1198066235@qq.com>
2014-11-15 02:02:45 +0800

    应用自定义控件并得到其选中值

--- app/src/main/java/me/linkcube/skea/ui/test/ReEvaluationActivity.java
+++ app/src/main/java/me/linkcube/skea/ui/test/ReEvaluationActivity.java
@@ -1,20 +1,107 @@
 package me.linkcube.skea.ui.test;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import me.linkcube.skea.R;
 import me.linkcube.skea.ui.BaseActivity;
+import me.linkcube.skea.view.LevelRadioGroup;
 
-public class ReEvaluationActivity extends BaseActivity {
+public class ReEvaluationActivity extends BaseActivity  {
+    //声明控件
+    private LevelRadioGroup reproduct_history_lrg;
+    private LevelRadioGroup sex_activity_lrg;
+    private LevelRadioGroup urinary_incontinence_lrg;
+    private LevelRadioGroup mental_status_lrg;
+
+    private Button submit_bt;
+
+    private EvaluationItemsClass mEvaluationItemsClass;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        init();
+
+
     }
+    /**初始化控件及变量，注册事件*/
+    private void init(){
+        //得到控件
+        reproduct_history_lrg=(LevelRadioGroup) findViewById(R.id.reproduct_history_lrg);
+        sex_activity_lrg=(LevelRadioGroup) findViewById(R.id.sex_activity_lrg);
+        urinary_incontinence_lrg=(LevelRadioGroup) findViewById(R.id.urinary_incontinence_lrg);
+        mental_status_lrg=(LevelRadioGroup) findViewById(R.id.mental_status_lrg);
+        submit_bt=(Button)findViewById(R.id.submit_bt);
+
+        //注册事件
+        reproduct_history_lrg.setOnOnLevelSelectedListener(new LevelRadioGroup.OnLevelSelectedListener() {
+            @Override
+            public void onLevelSelected(int level) {
+                Log.i("CXC", "reproduct_history_lrg---" + level);
+                //得到相应的Level
+                mEvaluationItemsClass.setReproductive_level(level);
+
+            }
+        });
+
+        sex_activity_lrg.setOnOnLevelSelectedListener(new LevelRadioGroup.OnLevelSelectedListener() {
+            @Override
+            public void onLevelSelected(int level) {
+                Log.i("CXC", "sex_activity_lrg---" + level);
+                mEvaluationItemsClass.setSexual_level(level);
+            }
+        });
+        urinary_incontinence_lrg.setOnOnLevelSelectedListener(new LevelRadioGroup.OnLevelSelectedListener() {
+            @Override
+            public void onLevelSelected(int level) {
+                Log.i("CXC", "urinary_incontinence_lrg---" + level);
+                mEvaluationItemsClass.setUrinary_level(level);
+            }
+        });
+        mental_status_lrg.setOnOnLevelSelectedListener(new LevelRadioGroup.OnLevelSelectedListener() {
+            @Override
+            public void onLevelSelected(int level) {
+                Log.i("CXC", "mental_status_lrg---" + level);
+                mEvaluationItemsClass.setMental_level(level);
+            }
+        });
+
+        submit_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+                /**
+                 *
+                 * 在这里得出测试结果
+                 *
+                 * **/
+
+
+                getResult();
+             }
+        });
+
+
+    }
+
+    /**
+     * 得到测试数据 并进行评诂
+     * */
+    private void getResult(){
+
+
+    }
+
 
     @Override
     public int getLayoutResourceId() {
         return R.layout.activity_re_evaluation;
     }
+
 
 }
