23e38d868e4c3799e4e85f8864421fd1c1b5d161
ervin-wang <orange.work@qq.com>
2014-11-16 14:03:42 +0800

    增加异常包

---
+++ app/src/main/java/me/linkcube/skea/exception/MyException.java
@@ -0,0 +1,7 @@
+package me.linkcube.skea.exception;
+
+/**
+ * Created by Ervin on 14/11/16.
+ */
+public class MyException {
+}
--- app/src/main/java/me/linkcube/skea/ui/bluetooth/BTSettingActivity.java
+++ app/src/main/java/me/linkcube/skea/ui/bluetooth/BTSettingActivity.java
@@ -22,7 +22,6 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
