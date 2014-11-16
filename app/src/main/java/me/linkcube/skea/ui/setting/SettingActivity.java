8294f9c8d17daece3a104d820aded723de3d1e96
ervin-wang <orange.work@qq.com>
2014-11-16 13:49:42 +0800

    删除无用资源文件

--- app/src/main/java/me/linkcube/skea/ui/setting/SettingActivity.java
+++ app/src/main/java/me/linkcube/skea/ui/setting/SettingActivity.java
@@ -1,15 +1,18 @@
 package me.linkcube.skea.ui.setting;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import me.linkcube.skea.R;
 import me.linkcube.skea.base.ui.BaseActivity;
 
-public class SettingActivity extends BaseActivity {
+public class SettingActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        findViewById(R.id.language).setOnClickListener(this);
     }
 
     @Override
@@ -17,4 +20,22 @@ public class SettingActivity extends BaseActivity {
         return R.layout.activity_setting;
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.language:
+                startActivity(new Intent(this, LanguageSettingActivity.class));
+                break;
+            case R.id.update:
+                break;
+
+            case R.id.feedback:
+                break;
+            case R.id.aboutus:
+                break;
+            default:
+                break;
+        }
+    }
 }
