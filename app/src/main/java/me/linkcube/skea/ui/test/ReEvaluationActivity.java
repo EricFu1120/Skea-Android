950221107aed0a5065bd2013c0ceb435ca9caed5
cxcbupt <1198066235@qq.com>
2014-11-17 09:53:17 +0800

    评诂测试时，日期，身高，体重选择控件

--- app/src/main/java/me/linkcube/skea/ui/test/ReEvaluationActivity.java
+++ app/src/main/java/me/linkcube/skea/ui/test/ReEvaluationActivity.java
@@ -6,28 +6,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import custom.android.app.dialog.DatePickerDialogFragment;
 import custom.android.app.dialog.SimpleDialogFragment;
 import me.linkcube.skea.R;
 import me.linkcube.skea.ui.BaseActivity;
+import me.linkcube.skea.ui.MainActivity;
 import me.linkcube.skea.view.LevelRadioGroup;
 
-public class ReEvaluationActivity extends BaseActivity  {
-    //声明控件
-    private LevelRadioGroup reproduct_history_lrg;
-    private LevelRadioGroup sex_activity_lrg;
-    private LevelRadioGroup urinary_incontinence_lrg;
-    private LevelRadioGroup mental_status_lrg;
+public class ReEvaluationActivity extends BaseActivity {
+//    //声明控件
+//    private LevelRadioGroup reproduct_history_lrg;
+//    private LevelRadioGroup sex_activity_lrg;
+//    private LevelRadioGroup urinary_incontinence_lrg;
+//    private LevelRadioGroup mental_status_lrg;
 
 
     private RelativeLayout birthday_layout;
     private RelativeLayout height_layout;
     private RelativeLayout weight_layout;
 
+
     private Button submit_bt;
 
     private EvaluationItemsClass mEvaluationItemsClass;
-
 
 
     @Override
@@ -37,53 +41,55 @@ public class ReEvaluationActivity extends BaseActivity  {
 
 
     }
-    /**初始化控件及变量，注册事件*/
-    private void init(){
+
+    /**
+     * 初始化控件及变量，注册事件
+     */
+    private void init() {
         //得到控件
-        reproduct_history_lrg=(LevelRadioGroup) findViewById(R.id.reproduct_history_lrg);
-        sex_activity_lrg=(LevelRadioGroup) findViewById(R.id.sex_activity_lrg);
-        urinary_incontinence_lrg=(LevelRadioGroup) findViewById(R.id.urinary_incontinence_lrg);
-        mental_status_lrg=(LevelRadioGroup) findViewById(R.id.mental_status_lrg);
-        submit_bt=(Button)findViewById(R.id.submit_bt);
+//        reproduct_history_lrg = (LevelRadioGroup) findViewById(R.id.reproduct_history_lrg);
+//        sex_activity_lrg = (LevelRadioGroup) findViewById(R.id.sex_activity_lrg);
+//        urinary_incontinence_lrg = (LevelRadioGroup) findViewById(R.id.urinary_incontinence_lrg);
+//        mental_status_lrg = (LevelRadioGroup) findViewById(R.id.mental_status_lrg);
+        submit_bt = (Button) findViewById(R.id.submit_bt);
 
-        birthday_layout=(RelativeLayout) findViewById(R.id.birthday_layout);
-        height_layout=(RelativeLayout) findViewById(R.id.height_layout);
-        weight_layout=(RelativeLayout) findViewById(R.id.weight_layout);
+        birthday_layout = (RelativeLayout) findViewById(R.id.birthday_layout);
+        height_layout = (RelativeLayout) findViewById(R.id.height_layout);
+        weight_layout = (RelativeLayout) findViewById(R.id.weight_layout);
 
 
-
-        //注册事件
-        reproduct_history_lrg.setOnOnLevelSelectedListener(new LevelRadioGroup.OnLevelSelectedListener() {
-            @Override
-            public void onLevelSelected(int level) {
-                Log.i("CXC", "reproduct_history_lrg---" + level);
-                //得到相应的Level
-                mEvaluationItemsClass.setReproductive_level(level);
-
-            }
-        });
-
-        sex_activity_lrg.setOnOnLevelSelectedListener(new LevelRadioGroup.OnLevelSelectedListener() {
-            @Override
-            public void onLevelSelected(int level) {
-                Log.i("CXC", "sex_activity_lrg---" + level);
-                mEvaluationItemsClass.setSexual_level(level);
-            }
-        });
-        urinary_incontinence_lrg.setOnOnLevelSelectedListener(new LevelRadioGroup.OnLevelSelectedListener() {
-            @Override
-            public void onLevelSelected(int level) {
-                Log.i("CXC", "urinary_incontinence_lrg---" + level);
-                mEvaluationItemsClass.setUrinary_level(level);
-            }
-        });
-        mental_status_lrg.setOnOnLevelSelectedListener(new LevelRadioGroup.OnLevelSelectedListener() {
-            @Override
-            public void onLevelSelected(int level) {
-                Log.i("CXC", "mental_status_lrg---" + level);
-                mEvaluationItemsClass.setMental_level(level);
-            }
-        });
+//        //注册事件
+//        reproduct_history_lrg.setOnOnLevelSelectedListener(new LevelRadioGroup.OnLevelSelectedListener() {
+//            @Override
+//            public void onLevelSelected(int level) {
+//                Log.i("CXC", "reproduct_history_lrg---" + level);
+//                //得到相应的Level
+//                mEvaluationItemsClass.setReproductive_level(level);
+//
+//            }
+//        });
+//
+//        sex_activity_lrg.setOnOnLevelSelectedListener(new LevelRadioGroup.OnLevelSelectedListener() {
+//            @Override
+//            public void onLevelSelected(int level) {
+//                Log.i("CXC", "sex_activity_lrg---" + level);
+//                mEvaluationItemsClass.setSexual_level(level);
+//            }
+//        });
+//        urinary_incontinence_lrg.setOnOnLevelSelectedListener(new LevelRadioGroup.OnLevelSelectedListener() {
+//            @Override
+//            public void onLevelSelected(int level) {
+//                Log.i("CXC", "urinary_incontinence_lrg---" + level);
+//                mEvaluationItemsClass.setUrinary_level(level);
+//            }
+//        });
+//        mental_status_lrg.setOnOnLevelSelectedListener(new LevelRadioGroup.OnLevelSelectedListener() {
+//            @Override
+//            public void onLevelSelected(int level) {
+//                Log.i("CXC", "mental_status_lrg---" + level);
+//                mEvaluationItemsClass.setMental_level(level);
+//            }
+//        });
 
         submit_bt.setOnClickListener(evaluationClickListener);
         birthday_layout.setOnClickListener(evaluationClickListener);
@@ -91,27 +97,37 @@ public class ReEvaluationActivity extends BaseActivity  {
         weight_layout.setOnClickListener(evaluationClickListener);
 
 
-
     }
 
     /**
      * 得到测试数据 并进行评诂
-     * */
-    private void getResult(){
+     */
+    private void getResult() {
 
 
     }
 
-    View.OnClickListener evaluationClickListener=new View.OnClickListener() {
+    View.OnClickListener evaluationClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            switch (v.getId()){
+            switch (v.getId()) {
                 case R.id.birthday_layout:
                     //todo
-
+                    //日期范围怎么设置
+                    DatePickerDialogFragment.createBuilder(getApplicationContext(), getSupportFragmentManager())
+                            .setDate(Calendar.getInstance().getTime())
+                            .setTitle("Birthday")
+                            .setPositiveButtonText("Yes")
+                            .setNegativeButtonText("Cancel")
+                            .show();
                     break;
                 case R.id.height_layout:
-
+                    SimpleDialogFragment.createBuilder(getApplicationContext(), getSupportFragmentManager())
+                            .setTitle("Title")
+                            .setMessage("this is message")
+                            .setPositiveButtonText("Yes")
+                            .setNegativeButtonText("No")
+                            .show();
                     break;
                 case R.id.weight_layout:
 
